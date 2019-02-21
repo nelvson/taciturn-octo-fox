47cb8bb29bbccb23d6271f55b4223381f66a58f6
nelvson <nelvson1337@gmail.com>
2019-02-21 10:51:19 +0700

    [api] messing around with server.js

--- api/createpackage.js
+++
@@ -1,7 +0,0 @@
-var dependencies = require('./package-lock.json').dependencies;
-var list = {};
-
-for (var p of Object.keys(dependencies)) {
-    list[p] = dependencies[p].version;
-}
-console.log(JSON.stringify(list, null, '  '));
