945014fc9bb2f4aa5158b708d54d4f57f1acec6c
nelvson <nelvson1337@gmail.com>
2019-02-22 18:01:25 +0700

    Initialise waterline

---
+++ api/src/waterline/waterline.ts
@@ -0,0 +1,16 @@
+import sailsMongoAdapter from 'sails-mongo';
+
+let config = {
+  adapters: {
+    'sails-mongo': sailsMongoAdapter,
+    //key-value pair
+  };
+
+  datastores: {
+    //key-value pair
+  },
+
+  models: {
+    //key-value pair
+  };
+};
